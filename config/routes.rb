297745c66b3e1bea415dd0bd87dc1891d7815212
Ty Jac <muzuuu@protonmail.com>
2024-03-09 11:59:36 +0800

    feat: add api version namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'current_user', to: 'current_user#index'
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
@@ -9,4 +8,11 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      get 'current_user', to: 'current_user#index'
+    end
+  end
 end
